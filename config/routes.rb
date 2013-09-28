de1bebb465c9578b31291fc2b74e8d75b99f5abc
Aayush Gupta <agx.coder@gmail.com>
2013-09-28 13:10:07 +0530

    Preparing profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+  
   
   root 'static_pages#home'
   match '/help', to: 'static_pages#help', via: 'get'
@@ -7,7 +7,7 @@ SampleApp::Application.routes.draw do
   match '/contact', to: 'static_pages#contact', via: 'get'
   
   match '/signup', to: 'users#new', via: 'get'
-
+  resources :users
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Aayush Gupta	"
+    email    "aayushgx@gmail.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
